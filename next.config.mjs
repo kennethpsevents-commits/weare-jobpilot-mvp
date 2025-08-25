af5ed46ee286cf713c696ce16df009d2e9efc28d
kennethpsevents-commits <kenneth.psevents@gmail.com>
2025-08-25 07:10:07 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
+// Fix ESM __dirname
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
