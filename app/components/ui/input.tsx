4267f08b3a94366610831b42516f9d8b4151d435
kennethpsevents-commits <kenneth.psevents@gmail.com>
2025-08-25 06:43:04 +0200

    Update input.tsx

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -1,16 +1,14 @@
 import * as React from "react";
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-export function Input({ className = "", ...props }: InputProps) {
+export default function Input({ className = "", ...props }: InputProps) {
   return (
     <input
       className={
-        "block w-full rounded-xl border px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-black/10 " + className
+        "block w-full rounded-md border px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-black/30 " +
+        className
       }
       {...props}
     />
   );
 }
-
-export default Input;
