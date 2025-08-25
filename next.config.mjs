194ba8cd59116d1a14f82e49278934a5a452e6c6
kennethpsevents-commits <kenneth.psevents@gmail.com>
2025-08-25 07:04:06 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
+
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: true }
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@/components": path.resolve(__dirname, "app/components"),
+      "@/lib": path.resolve(__dirname, "app/lib"),
+      "@/data": path.resolve(__dirname, "data"),
+    };
+    return config;
+  },
 };
+
 export default nextConfig;
