b09672eb5dbfe6637668b62c25b476ab3adb49b5
kennethpsevents-commits <kenneth.psevents@gmail.com>
2025-08-25 06:42:21 +0200

    Update button.tsx

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,17 +1,14 @@
 import * as React from "react";
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
-
-export function Button({ className = "", ...props }: ButtonProps) {
+export default function Button({ className = "", ...props }: ButtonProps) {
   return (
     <button
       className={
-        "inline-flex items-center justify-center rounded-xl border px-4 py-2 text-sm font-medium " +
-        "hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none " + className
+        "inline-flex items-center justify-center rounded-md px-4 py-2 text-sm font-medium border border-black/10 hover:border-black/20 focus:outline-none focus:ring-2 focus:ring-black/30 " +
+        className
       }
       {...props}
     />
   );
 }
-
-export default Button;
