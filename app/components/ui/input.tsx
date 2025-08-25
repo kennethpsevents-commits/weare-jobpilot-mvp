ae48f848afd84865b63b1a9dc54bd24d58a6d4ee
kennethpsevents-commits <kenneth.psevents@gmail.com>
2025-08-25 07:06:36 +0200

    Update input.tsx

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -5,7 +5,8 @@ export default function Input({ className = "", ...props }: InputProps) {
   return (
     <input
       className={
-        "block w-full rounded-md border px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-black/30 " +
+        "block w-full rounded-md border px-3 py-2 text-sm outline-none " +
+        "focus:ring-2 focus:ring-black/30 " +
         className
       }
       {...props}
