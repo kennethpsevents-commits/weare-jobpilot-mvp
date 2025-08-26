bd84ad9a57fcd1bde8d957e0f5c242c9a6364552
kennethpsevents-commits <kenneth.psevents@gmail.com>
2025-08-26 08:05:10 +0200

    Create route.ts

---
+++ app/api/ok/app/api/route.ts
@@ -0,0 +1,6 @@
+export const dynamic = "force-dynamic";
+export async function GET() {
+  return new Response(JSON.stringify({ ok: true, t: Date.now() }), {
+    headers: { "content-type": "application/json" },
+  });
+}
