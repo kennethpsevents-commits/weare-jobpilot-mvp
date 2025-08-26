e119da0d5c7c67ae3a2d229cc33dd5ab98f7a920
kennethpsevents-commits <kenneth.psevents@gmail.com>
2025-08-26 09:16:58 +0200

    Create Job.ts

---
+++ lib/Job.ts
@@ -0,0 +1,12 @@
+// lib/jobs.ts
+export const jobs = [
+  {
+    id: "demo-1",
+    title: "Junior Frontend Developer",
+    company: "JobPilot",
+    location: "Remote",
+    applyUrl: "https://wearejobpilot.com/apply?job=demo-1",
+  },
+];
+
+export default jobs;
