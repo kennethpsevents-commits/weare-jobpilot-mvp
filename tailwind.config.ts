f1cbc1d2123636eaa58541de9667a6ef2bfbd3ef
kennethpsevents-commits <kenneth.psevents@gmail.com>
2025-08-29 12:47:43 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./pages/**/*.{ts,tsx}",
-  ],
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./pages/**/*.{ts,tsx}"],
   theme: { extend: {} },
   plugins: [],
 } satisfies Config;
