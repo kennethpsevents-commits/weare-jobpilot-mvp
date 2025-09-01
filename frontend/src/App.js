3536da67200f02bd13c3ff6f3f2e2a0e72a659cb
kennethpsevents-commits <kenneth.psevents@gmail.com>
2025-09-01 08:21:37 +0200

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import JobPilot from "./components/JobPilot";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <JobPilot />
+    </div>
+  );
+}
